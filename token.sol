bd7ef08a0cd6e3f778c3665fe93cfbf32347b6ba
Victor Tyavbee <68505976+Iamtsquare07@users.noreply.github.com>
2023-10-05 07:41:50 +0100

    Add files via upload

---
+++ token.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract EtherusdGang is ERC20, Pausable, Ownable, ERC20Permit, ERC20Votes {
+    constructor() ERC20("Etherusd Gang", "EG") ERC20Permit("Etherusd Gang") {
+        // Set the total supply to 1 million tokens (1,000,000 * 10^18)
+        _mint(msg.sender, 7_000_000 * 10**18);
+    }
+
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        require(
+            totalSupply() + amount <= 7_000_000 * 10**18,
+            "Exceeds total supply limit"
+        );
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+
+}
