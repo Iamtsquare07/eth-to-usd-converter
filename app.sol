d8e04e058736bac7cd6524a52f41ce9bbee95b8d
Victor Tyavbee <68505976+Iamtsquare07@users.noreply.github.com>
2023-10-05 07:40:25 +0100

    Add files via upload

--- app.sol
+++ app.sol
@@ -1,21 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract EtherusdGang is ERC20, Pausable, Ownable, ERC20Permit, ERC20Votes {
-    constructor() ERC20("Etherusd Gang", "EG") ERC20Permit("Etherusd Gang") {
-        superAdmin = msg.sender;
-        admins[msg.sender] = true; // Make the super admin an admin by default
-        adminAddresses.push(superAdmin);
-
-        // Set the total supply to 1 million tokens (1,000,000 * 10^18)
-        _mint(msg.sender, 7_000_000 * 10**18);
-    }
+contract EtherustContract {
+    IERC20 public etherUSDToken;
+    address public owner;
 
     // Super admin address
     address private superAdmin;
@@ -26,6 +16,14 @@ contract EtherusdGang is ERC20, Pausable, Ownable, ERC20Permit, ERC20Votes {
     // Array to keep track of admin proposals from users
     AdminProposal[] public adminProposals;
 
+    constructor(address _tokenAddress) {
+        etherUSDToken = IERC20(_tokenAddress);
+        owner = msg.sender;
+        superAdmin = msg.sender;
+        admins[msg.sender] = true; // Make the super admin an admin by default
+        adminAddresses.push(superAdmin);
+    }
+
     struct AdminProposal {
         address proposedAdmin;
         uint256 supportVotes;
@@ -33,6 +31,7 @@ contract EtherusdGang is ERC20, Pausable, Ownable, ERC20Permit, ERC20Votes {
         bool executed;
     }
 
+    uint8 minTokensRequired = 7;
     // Modifier to restrict access to the super admin
     modifier onlySuperAdmin() {
         require(
@@ -104,70 +103,37 @@ contract EtherusdGang is ERC20, Pausable, Ownable, ERC20Permit, ERC20Votes {
     }
 
     function voteForAdminProposal(uint256 _proposalId, bool support) public {
-        require(_proposalId < adminProposals.length, "Invalid proposal ID.");
-        require(
-            !adminProposals[_proposalId].executed,
-            "Proposal has already been executed."
-        );
-        require(
-            !hasVoted[msg.sender][_proposalId],
-            "You have already voted for this proposal."
-        );
+    require(_proposalId < adminProposals.length, "Invalid proposal ID.");
+    require(
+        !adminProposals[_proposalId].executed,
+        "Proposal has already been executed."
+    );
+    require(
+        !hasVoted[msg.sender][_proposalId],
+        "You have already voted for this proposal."
+    );
 
-        // Mark the user as voted for this proposal
-        hasVoted[msg.sender][_proposalId] = true;
+    // Get the user's token balance directly from the token contract
+    uint256 userTokenBalance = etherUSDToken.balanceOf(msg.sender);
 
-        // Update the vote counts based on whether the user supports or opposes
-        if (support) {
-            adminProposals[_proposalId].supportVotes += 1;
-        } else {
-            adminProposals[_proposalId].opposeVotes += 1;
-        }
+    // Check if the user has a minimum number of tokens (adjust the value accordingly)
+    require(
+        userTokenBalance >= minTokensRequired,
+        "Insufficient tokens."
+    );
+
+    // Mark the user as voted for this proposal
+    hasVoted[msg.sender][_proposalId] = true;
+
+    // Update the vote counts based on whether the user supports or opposes
+    if (support) {
+        adminProposals[_proposalId].supportVotes += 1;
+    } else {
+        adminProposals[_proposalId].opposeVotes += 1;
     }
+}
 
-    function pause() public onlySuperAdmin {
-        _pause();
-    }
 
-    function unpause() public onlySuperAdmin {
-        _unpause();
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override whenNotPaused {
-        super._beforeTokenTransfer(from, to, amount);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override(ERC20, ERC20Votes) {
-        super._afterTokenTransfer(from, to, amount);
-    }
-
-    function _mint(address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
-        require(
-            totalSupply() + amount <= 7_000_000 * 10**18,
-            "Exceeds total supply limit"
-        );
-        super._mint(to, amount);
-    }
-
-    function _burn(address account, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
-        super._burn(account, amount);
-    }
 
     // Function to get the number of upvotes and downvotes for a proposal
     function getVotesForProposal(uint256 proposalId)
@@ -180,4 +146,10 @@ contract EtherusdGang is ERC20, Pausable, Ownable, ERC20Permit, ERC20Votes {
         AdminProposal storage proposal = adminProposals[proposalId];
         return (proposal.supportVotes, proposal.opposeVotes);
     }
+
+    // Function to get the balance of a user in EtherUSD tokens
+    function getUserBalance(address user) public view returns (uint256) {
+        return etherUSDToken.balanceOf(user);
+    }
+
 }
